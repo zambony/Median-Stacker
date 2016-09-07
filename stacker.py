5c8ae2b93a5ca33d168c672bb949458b56c67298
Z0mbine <Z0mbine@users.noreply.github.com>
2016-09-07 10:49:07 -0700

    Removed semicolons

--- stacker.py
+++ stacker.py
@@ -1,23 +1,23 @@
-from PIL import Image;
-from os import listdir;
-import numpy;
+from PIL import Image
+from os import listdir
+import numpy
 
 def main():
 	# Create an array to hold all of our images
-	images = [];
+	images = []
 
 	# For every file inside our working directory...
 	for fileName in (listdir("images\\")):
 		# Open the image and create an array from it, then append that image array into our image list
-		images.append(numpy.array(Image.open("images\\" + fileName)));
+		images.append(numpy.array(Image.open("images\\" + fileName)))
 
 	# Use the built-in median function inside numpy to find the median image, and convert all values to uint8 (0 - 255)
-	stacked = numpy.uint8(numpy.median(images, 0));
+	stacked = numpy.uint8(numpy.median(images, 0))
 
 	# Use Pillow to create an image from an array of data
-	output = Image.fromarray(stacked);
+	output = Image.fromarray(stacked)
 
 	# Save it as a PNG for max quality
-	output.save("stacked.png", "PNG");
+	output.save("stacked.png", "PNG")
 
-main();
+main()
